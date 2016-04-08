a0198a33a7eb99dc76f11018fe6a37f5d5a31f10
espejor <espejor@gmail.com>
2016-04-08 22:58:17 +0200

    no message

--- javascript/accesoDatos.js
+++ javascript/accesoDatos.js
@@ -274,6 +274,7 @@ function getMes(mesNum){
 function crearMapa(){
   // Create a map object and specify the DOM element for display.
   $('#contenido').css("height","100%");
+  $('#contenido').css("width","100%");
   var map = new google.maps.Map(document.getElementById('contenido'), {
     center: {lat: parseFloat(latitud), lng: parseFloat(longitud)},
     scrollwheel: false,
